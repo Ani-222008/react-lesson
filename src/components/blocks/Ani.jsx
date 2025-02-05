1b718b13bc3069a5cd765862bba2e5ced7eb9efc
Ani <anih32129@gmail.com>
2025-02-05 19:16:39 +0400

    created a new component

---
+++ src/components/blocks/Ani.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ani() {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit perferendis vel officia assumenda excepturi neque similique facilis tempora amet, iusto labore eveniet, ipsam minus sequi, dolorem aliquam eius alias quidem!
+    </div>
+  )
+}
