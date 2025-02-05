a4a7dfd272f88ede473519704d4a2a071750fd3f
Ani <anih32129@gmail.com>
2025-02-05 19:08:42 +0400

    Add new page

---
+++ src/components/blocks/Ani.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Ani() {
+  return (
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid inventore quasi ipsum amet mollitia eligendi, consectetur accusantium totam unde ipsa? Illum amet harum reiciendis mollitia asperiores sit repellendus hic aliquam.</div>
+  )
+}
